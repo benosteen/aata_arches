6bb06a77fabf3505bd102f0f4a2301eb85e9d206
benosteen <bosteen@gmail.com>
2019-04-02 10:21:08 -0700

    Adding Postgres setup SQL and init script

---
+++ postgres/setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Creating an array that defines the environment variables
+# that must be set. This can be consumed later via arrray
+# variable expansion ${REQUIRED_ENV_VARS[@]}.
+readonly REQUIRED_ENV_VARS=(
+  "PGUSERNAME"
+  "PGPASSWORD"
+  "PGDBNAME"
+  "POSTGRES_USER")
+
+main() {
+  check_env_vars_set
+  init_user_and_db
+}
+
+check_env_vars_set() {
+  for required_env_var in ${REQUIRED_ENV_VARS[@]}; do
+    if [[ -z "${!required_env_var}" ]]; then
+      echo "Error:
+    Environment variable '$required_env_var' not set.
+    Make sure you have the following environment variables set:
+      ${REQUIRED_ENV_VARS[@]}
+Aborting."
+      exit 1
+    fi
+  done
+}
+
+
+# Performs the initialization in the already-started PostgreSQL
+# using the preconfigured POSTGRE_USER user.
+init_user_and_db() {
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+     CREATE USER $PGUSERNAME WITH PASSWORD '$PGPASSWORD';
+     CREATE DATABASE $PGDBNAME TEMPLATE template_postgis_20;
+     GRANT ALL PRIVILEGES ON DATABASE $PGDBNAME TO $PGUSERNAME;
+EOSQL
+}
+
+# Executes the main routine with environment variables
+# passed through the command line. We don't use them in
+# this script but now you know 🤓
+main "$@"
